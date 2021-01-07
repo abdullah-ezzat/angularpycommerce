33e624363d710593e7e676279f5f42dc859e39ba
MastergamingYT <abdullahmohamedezzat21@gmail.com>
2021-01-07 20:43:43 +0200

    deploy

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 app.use(express.static('./dist/AngularProjectNew'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/AngularProjectNew/'}),
-);
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/AngularProjectNew' }
+  );
+});
 
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
