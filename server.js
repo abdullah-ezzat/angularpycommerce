e590bc3736d7d5c4cdf398876f9ed1b6ed12e353
MastergamingYT <abdullahmohamedezzat21@gmail.com>
2021-01-07 19:37:34 +0200

    deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/AngularProjectNew'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/AngularProjectNew/index.html'));});
+
+app.use(express.static('./dist/AngularProjectNew'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/AngularProjectNew/'}),
+);
+
 app.listen(process.env.PORT || 8080);
