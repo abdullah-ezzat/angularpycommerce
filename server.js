9c6bfc5a84077090763a15e69151e7ebf40b0d4e
MastergamingYT <abdullahmohamedezzat21@gmail.com>
2021-06-29 00:12:35 +0200

    allowed cors

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/angularprojectnew/index.html'));});
 app.listen(process.env.PORT || 8080);
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+  });
